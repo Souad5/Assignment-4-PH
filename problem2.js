ac7fc23c427849fe0de6afd06f906fdac9f8a797
Souad5 <souadalkabir@gmail.com>
2025-02-17 20:30:18 +0600

    problem 2

---
+++ problem2.js
@@ -0,0 +1,31 @@
+function validEmail(email) {
+    if (typeof email !== 'string') {
+        return "Invalid";
+    }
+    
+    let specialChars = "._+-@";
+    if (specialChars.includes(email[0])) {
+        return false;
+    }
+    
+    if (!email.includes("@")) {
+        return false;
+    }
+    
+    if (email.indexOf("@") === 0 || email.indexOf("@") === email.length - 1) {
+        return false;
+    }
+    
+    if (email.includes(" ")) {
+        return false;
+    }
+    
+    if (!email.endsWith(".com")) {
+        return false;
+    }
+    
+    return true;
+}
+
+let result = validEmail("jhankar@hero.com");
+console.log(result);
